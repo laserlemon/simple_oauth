65f01e0c925d669417eb06efedf15e8bc6f940bf
Erik Michaels-Ober <sferik@gmail.com>
2011-05-06 13:00:41 -0700

    Apparently, Object#tap was added in 1.8.7, not 1.9

--- lib/simple_oauth/core_ext/object.rb
+++ lib/simple_oauth/core_ext/object.rb
@@ -1,6 +1,6 @@
 major, minor, patch = RUBY_VERSION.split('.')
 
-if major.to_i == 1 && minor.to_i < 9
+if major.to_i == 1 && minor.to_i == 8 && patch.to_i <= 6
   class Object
     def tap
       yield self
