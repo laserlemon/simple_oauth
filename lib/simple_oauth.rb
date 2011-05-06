7aec6fe2bae559e7b89fa609261b7a7236d4589e
Erik Michaels-Ober <sferik@gmail.com>
2011-05-06 12:18:55 -0700

    Trust the load path

--- lib/simple_oauth.rb
+++ lib/simple_oauth.rb
@@ -2,5 +2,5 @@ require 'base64'
 require 'cgi'
 require 'openssl'
 require 'uri'
-require File.expand_path('../simple_oauth/core_ext/object', __FILE__)
-require File.expand_path('../simple_oauth/header', __FILE__)
+require 'simple_oauth/core_ext/object'
+require 'simple_oauth/header'
